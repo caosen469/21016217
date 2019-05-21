eee751b2b2f0800401699c95b97a7923dad08786
Caosen98 <sihancao496@gmail.com>
2019-05-21 17:22:17 +0800

    finish the proj0 !!

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -51,6 +51,48 @@ public class NBody
 			planets[i].draw();
 		}
 
-		enableDoubleBuffering();
+		StdDraw.enableDoubleBuffering();
+/**---------------------------------------------------------------------------------*/
+/*start the animation**/
+
+	   	int timeRecord = 0; /**just represents the time*/
+
+	   	while(timeRecord < T)
+	   	{
+	   		/**store the forces exerted on each planet*/
+	   		Double xForces[] = new Double[numberOfPlanets]; 
+			Double yForces[] = new Double[numberOfPlanets];
+
+	   		for(int i=0; i<numberOfPlanets; i++)
+	   		{
+	   			xForces[i] = planets[i].calcNetForceExertedByX(planets);
+	   			yForces[i] = planets[i].calcNetForceExertedByY(planets);
+	   		}
+	   		for(int i=0;i<numberOfPlanets;i++)
+	   		{
+	   			planets[i].update(dt, xForces[i],yForces[i]);
+	   		}
+
+	   		StdDraw.picture(0, 0, "images/starfield.jpg");
+
+	   		for(int i =0; i<numberOfPlanets; i++)
+	   		{
+	   			planets[i].draw();
+	   		}
+
+	   		StdDraw.show();
+	   		StdDraw.pause(10);
+
+	   		timeRecord += dt;
+	   	}
+
+	   	StdOut.printf("%d\n", planets.length);
+	   	StdOut.printf("%.2e\n", radius);
+	   	for(int i=0; i<planets.length; i++)
+	   	{
+	   		StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+	   			planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+	   			planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
+	   	}
 	}
 }
